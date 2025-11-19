b494bde4a11d3cbd9cd4d96ac076369d32004fa4
jonasmd23 <jonasmd@byu.edu>
2025-11-19 13:16:04 -0700

    Create README.m

---
+++ README.m
@@ -0,0 +1,11 @@
+git add --all
+git commit -m "(completed tasks)"
+git push
+
+
+ATARI PROCESS
+1. implement renderings
+2. implement ball state machine
+3. implement platform movement
+4. implement brick interactions
+5. cry
